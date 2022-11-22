a132ae59f8a75214d086a2d9a9e43c83ce79e645
Ysrael Baruque <israekbaruque992@gmail.com>
2022-11-22 08:49:39 -0300

    adicionando telas

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Menu = createBottomTabNavigator().Navigator;
+const ItenMenu = createBottomTabNavigator().Screen;
 
 export default function App() {
   return (
     <NavigationContainer>
-      
+      <Menu>
+        <ItenMenu name="FontAwesome" component={}/>
+      </Menu>
     </NavigationContainer>
   );
 }
