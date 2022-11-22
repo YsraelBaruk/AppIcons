e80a6c90838985861b0cec1fe400dc60e5432e59
Ysrael Baruque <israekbaruque992@gmail.com>
2022-11-22 09:02:08 -0300

    biblioteca icones

--- components/TelaFontAwesome.js
+++ components/TelaFontAwesome.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function FontAwesome() {
   return (
