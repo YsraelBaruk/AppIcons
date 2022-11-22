966b315e7bb9152942b4b8f8145c3b3b481750df
Ysrael Baruque <israekbaruque992@gmail.com>
2022-11-22 08:54:21 -0300

    mais telas

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import FontAwesome from "./components/FontAwesome";
+import Zocial from "./components/Zocial";
+import SimpleLineIcons from "./components/SimpleLineIcons";
+
 const Menu = createBottomTabNavigator().Navigator;
 const ItenMenu = createBottomTabNavigator().Screen;
 
@@ -8,7 +12,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Menu>
-        <ItenMenu name="FontAwesome" component={}/>
+        <ItenMenu name="FontAwesome" component={FontAwesome}/>
+        <ItenMenu name="Zocial" component={Zocial}/>
+        <ItenMenu name="SimpleLineIcons" component={SimpleLineIcons}/>
       </Menu>
     </NavigationContainer>
   );
